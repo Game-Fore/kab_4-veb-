a8c6dd8deb841f89b8f8c7bdeab37671c61f9be7
MSI Home <MSI Home@MSI-HOME>
2026-01-31 16:40:25 +1000

    Initial commit

--- src/Controller/CookieController.php
+++ src/Controller/CookieController.php
@@ -4,25 +4,24 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Cookie;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class CookieController extends AbstractController
 {
     #[Route('/cookie/accept', name: 'cookie_accept', methods: ['POST'])]
-    public function accept(Request $request): RedirectResponse
+    public function accept(Request $request): Response
     {
-        $response = new RedirectResponse($request->headers->get('referer') ?? '/');
+        $response = $this->redirect($request->headers->get('referer') ?? '/');
 
-        // cookie_apply = true на 30 дней
-        $cookie = new Cookie(
-            'cookie_apply',
-            'true',
-            strtotime('+30 days')
+        $response->headers->setCookie(
+            new Cookie(
+                'cookie_apply',
+                'true',
+                time() + 60 * 60 * 24 * 30 // 30 дней
+            )
         );
-
-        $response->headers->setCookie($cookie);
 
         return $response;
     }
